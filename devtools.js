9d51e7c816922315443d09b979c6d3aa35bd9a23
syb-illin <35152171+syb-illin@users.noreply.github.com>
2018-04-18 18:54:54 +0200

    Update devtools.js

--- devtools.js
+++ devtools.js
@@ -6,3 +6,8 @@ chrome.devtools.panels.create("Flow",
       // code invoked on panel creation
     }
 );
+chrome.devtools.panels.elements.createSidebarPane("My Sidebar",
+    function(sidebar) {
+        // sidebar initialization code here
+        sidebar.setObject({ some_data: "Some data to show" });
+});
